b64769ab2e2498ef5e9ab47aedef1dbacd69dc66
paoloconza <paolocoha@gmail.com>
2023-12-14 04:24:10 -0500

    estilo

--- resources/views/crear.blade.php
+++ resources/views/crear.blade.php
@@ -10,8 +10,10 @@
 
 <body class="flex justify-center h-screen items-center flex-wrap">
     <div class="flex flex-col items-center">
-        <a class="bg-[034b6a]" href="/usuario">Volver al listado</a>
-        <h1 class="text-4xl mb-8 font-bold">Nuevo usuario</h1>
+        <div class="flex mb-8">
+            <h1 class="text-xl font-bold mr-16">Nuevo usuario</h1>
+            <a class="border bg-[#034b6a] text-white px-2 py-0.5 rounded-md" href="/usuario">Volver al listado</a>
+        </div>
         <form class="w-full" action="api/usuarios" method="post">
             <div class="flex flex-col">
                 <label class="text-slate-400" for="nombre">Nombres</label>
@@ -22,7 +24,7 @@
                 <input class="border-2 rounded-lg p-2" type="text" name="apellido">
             </div>
             <div class="flex flex-col">
-                <label class="text-slate-400" for="correo">Correo</label>
+                <label class="text-slate-400" for="correo_electronico">Correo</label>
                 <input class="border-2 rounded-lg p-2" type="text" name="correo_electronico">
             </div>
             <div class="flex flex-col">
